02dd4d283c37c32af3c81b1f090f9ba8dbb5cbc3
alissonfpmorais <alissonfpmorais@gmail.com>
2018-11-08 14:00:59 -0200

    add basic auth controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,12 +1,35 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
+  alias Discuss.User
 
-  def callback(conn, params) do
-    IO.puts "+++++"
-    IO.inspect conn.assigns
-    IO.puts "+++++"
-    IO.inspect params
-    IO.puts "+++++"
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"code" => _code, "provider" => provider}) do
+    response =
+      %{name: auth.info.name, email: auth.info.email, token: auth.credentials.token, provider: provider}
+      |> insert_or_update_user
+
+    sign_in(conn, response)
+  end
+
+  defp insert_or_update_user(%{email: email} = user_params) do
+    case Repo.get_by(User, email: email) do
+      nil -> User.changeset(%User{}, user_params)
+      user -> User.changeset(user, user_params)
+    end
+    |> Repo.insert_or_update
+  end
+
+  defp sign_in(conn, response) do
+    case response do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 end
